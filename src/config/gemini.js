bb0da822c95ef8452948d960c2d652b72829624f
p-harshit1104 <harshitp110604@gmail.com>
2025-03-24 00:47:38 +0530

    message

---
+++ src/config/gemini.js
@@ -0,0 +1,39 @@
+
+
+
+
+import {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  } from "@google/generative-ai";
+  
+  const apiKey = "AIzaSyDwjGNn5356_etKD2jbMOKew4mZBvXnT7w";
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-2.0-flash",
+  });
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 40,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+  
+  async function run(prompt) {
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage(prompt);
+    const response=result.response;
+    console.log(result.response.text());
+    return response.text();
+  }
+  
+  export default run;
